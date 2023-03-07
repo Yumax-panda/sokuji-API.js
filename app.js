522ae3d459c448d27fa9aed97729e44454e8bfd5
Yumax-panda <93650251+Yumax-panda@users.noreply.github.com>
2023-03-07 19:59:13 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Deta } = require('deta');
+
+const deta = Deta('PROJECT_KEY')
+const db = deta.Base('sokuji')
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json())
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+app.get('/user/:user_id', async (req, res) => {
+    const { user_id } = req.params;
+    const user = await db.get(user_id);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json(null);
+    }
+});
