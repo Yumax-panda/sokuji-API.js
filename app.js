c9f2ab1b12611b529f6b1dfb1245ec6ae1b3ed7e
Yumax-panda <93650251+Yumax-panda@users.noreply.github.com>
2023-04-19 19:49:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { Deta } = require('deta');
 
 const deta = Deta('PROJECT_KEY')
 const db = deta.Base('sokuji')
+const result_db = deta.Base('results');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -17,6 +18,17 @@ app.get('/user/:user_id', async (req, res) => {
     const user = await db.get(user_id);
     if (user) {
         res.json(user);
+    } else {
+        res.status(404).json(null);
+    }
+});
+
+app.get('/results/:result_id', async (req, res) => {
+    res.set({'Access-Control-Allow-Origin': '*'});
+    const { result_id } = req.params;
+    const result = await result_db.get(result_id);
+    if (result) {
+        res.json(result);
     } else {
         res.status(404).json(null);
     }
